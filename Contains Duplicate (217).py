546ddf2cd57c33ccb9c73c1ed729bead456fc18a
Yosi Stern <yossi54321@gmail.com>
2023-08-23 21:21:10 +0300

    add file for 1920 Build Array from Permutation

--- Contains Duplicate (217).py
+++ Contains Duplicate (217).py
@@ -5,7 +5,7 @@ element is distinct.
 """
 
 
-def containsDuplicate(nums):
+def contains_duplicate(nums):
     exist = set()
     for num in nums:
         if num in exist:
@@ -16,7 +16,7 @@ def containsDuplicate(nums):
 
 
 if '__main__' == __name__:
-    assert containsDuplicate([1, 2, 3, 1]) == True
-    assert containsDuplicate([1, 2, 3, 4]) == False
-    assert containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True
+    assert contains_duplicate([1, 2, 3, 1]) == True
+    assert contains_duplicate([1, 2, 3, 4]) == False
+    assert contains_duplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]) == True
     print('All tests passed!')
