d2c2a474d4b8519b657db840effbb39a0fc16142
Yosi Stern <yossi54321@gmail.com>
2023-08-27 16:47:29 +0300

    1. Two Sum

---
+++ 1. Two Sum.py
@@ -0,0 +1,27 @@
+"""
+1. Two Sum
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+"""
+
+
+def two_sum(nums: list[int], target: int) -> list[int]:
+    i = 0
+    nums_values = {}
+    for num in nums:
+        nums_values[num] = i
+        i += 1
+    i = 0
+    for num in nums:
+        if nums_values.get(target - num)  and i != nums_values.get(target - num):
+            return [i, nums_values.get(target - num)]
+        i += 1
+
+
+if '__main__' == __name__:
+    assert two_sum([2, 7, 11, 15], 9) == [0, 1]
+    assert two_sum([3, 2, 4], 6) == [1, 2]
+    assert two_sum([3, 3], 6) == [0, 1]
+    print('All tests passed!')
